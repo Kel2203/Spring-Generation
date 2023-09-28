b3c04e912f20a703498b94ea109880bfb16f7b4e
kel2203 <raquelsb2001@gmail.com>
2023-09-28 15:22:32 -0300

    bug fixing

--- hello-world/hello-word/src/main/java/com/example/helloworld/controller/HelloWolrdController.java
+++ hello-world/hello-word/src/main/java/com/example/helloworld/controller/HelloWolrdController.java
@@ -6,8 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
-
-
+//teste github
 @RestController
 @RequestMapping("/exercicio-01")
 public class HelloWolrdController {
