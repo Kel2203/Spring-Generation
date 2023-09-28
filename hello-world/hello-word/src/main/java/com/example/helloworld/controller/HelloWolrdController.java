b9b5e3c1560fb318eb848315768410358c6185c8
kel2203 <raquelsb2001@gmail.com>
2023-09-28 13:01:38 -0300

    exercicio 01 - Spring

---
+++ hello-world/hello-word/src/main/java/com/example/helloworld/controller/HelloWolrdController.java
@@ -0,0 +1,41 @@
+package com.example.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/exercicio-01")
+public class HelloWolrdController {
+
+    @GetMapping("/hello-world")
+    public String hello(){
+        return "Hello, World!!";
+    }
+    @GetMapping("/bsm")
+    public String bsm(){
+        return("Persistência\n" +
+                "Mentalidade de Crescimento\n" +
+                "Orientação ao Futuro\n" +
+                "Responsabilidade Pessoal\n" +
+                "Trabalho em Equipe\n" +
+                "Comunicação\n" +
+                "Orientação aos Detalhes\n" +
+                "Proatividade");
+    }
+    @GetMapping("/objetivos")
+    public ArrayList<String> obj(){
+
+        return(new ArrayList<>(List.of("Quero melhorar minhas habilidades em: ",
+                "Trabalho em Equipe\n" ,
+                "Comunicação\n",
+                "Orientação aos Detalhes\n",
+                "Proatividade")));
+    }
+
+
+}
---
+++ hello-world/hello-word/src/test/java/com/example/helloword/HelloWordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
